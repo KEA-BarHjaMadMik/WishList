072d6abed0d95f0588f324da04b1ec7ff65e524a
Mbroholm <mibr0005@stud.ek.dk>
2025-11-03 16:01:18 +0100

    added repository tests

--- src/test/java/com/example/wishlist/repository/WishListRepositoryTest.java
+++ src/test/java/com/example/wishlist/repository/WishListRepositoryTest.java
@@ -1,5 +1,109 @@
 package com.example.wishlist.repository;
 
+import com.example.wishlist.model.WishItem;
+import com.example.wishlist.model.WishList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(scripts = "classpath:h2init.sql", executionPhase = BEFORE_TEST_METHOD)
 class WishListRepositoryTest {
 
+    @Autowired
+    private WishListRepository repository;
+
+    @Test
+    void shouldGetUserWishLists() {
+        String username = "test_wisher";
+
+        List<WishList> wishLists = repository.getUserWishLists(username);
+
+        assertThat(wishLists).isNotNull();
+
+        // test size
+        assertThat(wishLists.size()).isGreaterThanOrEqualTo(2);
+
+        // test id
+        assertThat(wishLists.get(0).getId()).isEqualTo(1);
+        assertThat(wishLists.get(1).getId()).isEqualTo(2);
+
+        // test title
+        assertThat(wishLists.get(0).getTitle()).isEqualTo("simpel");
+        assertThat(wishLists.get(1).getTitle()).isEqualTo("komplet");
+        assertThat(wishLists.get(2).getTitle()).isEqualTo("privat");
+
+        // test username is correct for all
+        for(WishList wishList : wishLists){
+            assertThat(wishList.getUsername()).isEqualTo(username);
+        }
+
+        // test description
+        assertThat(wishLists.get(0).getDescription()).isNull();
+        assertThat(wishLists.get(1).getDescription()).isNotNull();
+
+        // test eventdate
+        assertThat(wishLists.get(0).getEventDate()).isNull();
+        assertThat(wishLists.get(1).getEventDate()).isEqualTo(LocalDate.of(2025, 12, 24));
+
+        // test notPublic
+        assertThat(wishLists.get(0).isNotPublic()).isFalse();
+        assertThat(wishLists.get(1).isNotPublic()).isFalse();
+        assertThat(wishLists.get(2).isNotPublic()).isTrue();
+    }
+
+    @Test
+    void shouldGetEmptyUserWishLists() {
+        String username = "test_wishee";
+
+        List<WishList> wishLists = repository.getUserWishLists(username);
+
+        assertThat(wishLists).isNotNull();
+
+        // test size
+        assertThat(wishLists).isEmpty();
+    }
+
+    @Test
+    void shouldGetWishList() {
+        WishList wishList = repository.getWishList("1");
+
+        assertThat(wishList).isNotNull();
+        assertThat(wishList.getTitle()).isEqualTo("simpel");
+    }
+
+    @Test
+    void shouldGetNullWishList() {
+        WishList wishList = repository.getWishList("0");
+
+        assertThat(wishList).isNull();
+    }
+
+    @Test
+    void shouldGetWishListItems() {
+        List<WishItem> wishItems = repository.getWishListItems("1");
+
+        assertThat(wishItems).isNotNull();
+
+        assertThat(wishItems).isNotEmpty();
+
+        assertThat(wishItems.size()).isGreaterThanOrEqualTo(2);
+    }
+
+    @Test
+    void shouldGetEmptyWishListItems() {
+        List<WishItem> wishItems = repository.getWishListItems("0");
+
+        assertThat(wishItems).isNotNull();
+        assertThat(wishItems).isEmpty();
+    }
 }
--- src/test/resources/h2init.sql
+++ src/test/resources/h2init.sql
@@ -1,104 +1,116 @@
-DROP DATABASE IF EXISTS wishlist_db;
-CREATE DATABASE wishlist_db;
-USE wishlist_db;
-
-CREATE TABLE user_account (
-                              username VARCHAR(15) NOT NULL,
-                              password VARCHAR(30) NOT NULL,
-                              email VARCHAR(254) NOT NULL,
-                              PRIMARY KEY (username),
-                              UNIQUE(email)
-);
-
-CREATE TABLE wish_list (
-                           id INT NOT NULL AUTO_INCREMENT,
-                           username VARCHAR(15) NOT NULL,
-                           title VARCHAR(100) NOT NULL,
-                           description VARCHAR(800) NULL,
-                           eventdate DATE NULL,
-                           not_public BOOLEAN NOT NULL DEFAULT 0,
-                           PRIMARY KEY (id),
-                           CONSTRAINT fk_wish_list_user_account FOREIGN KEY (username)
-                               REFERENCES user_account(username)
-                               ON DELETE CASCADE
-                               ON UPDATE CASCADE
-);
-
-CREATE TABLE wish_item (
-                           id INT NOT NULL AUTO_INCREMENT,
-                           wish_list_id INT NOT NULL,
-                           title VARCHAR(100) NOT NULL,
-                           favourite BOOLEAN NOT NULL DEFAULT 0,
-                           description VARCHAR(800) NULL,
-                           price DECIMAL(10,2) NULL,
-                           quantity INT NOT NULL DEFAULT 1,
-                           link VARCHAR(400) NULL,
-                           reserved BOOLEAN NOT NULL DEFAULT 0,
-                           reserved_by VARCHAR(15) NULL,
-                           PRIMARY KEY (id),
-                           CONSTRAINT fk_wish_list_id FOREIGN KEY (wish_list_id)
-                               REFERENCES wish_list (id)
-                               ON DELETE CASCADE
-                               ON UPDATE CASCADE,
-                           CONSTRAINT fk_wish_item_reserved_by FOREIGN KEY (reserved_by)
-                               REFERENCES user_account (username)
-                               ON DELETE SET NULL
-                               ON UPDATE CASCADE
-);
-
-USE wishlist_db;
-
 SET FOREIGN_KEY_CHECKS = 0;
-TRUNCATE TABLE user_account;
-TRUNCATE TABLE wish_list;
-TRUNCATE TABLE wish_item;
+DROP TABLE IF EXISTS wish_item;
+DROP TABLE IF EXISTS wish_list;
+DROP TABLE IF EXISTS user_account;
 SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE user_account
+(
+    username VARCHAR(15)  NOT NULL,
+    password VARCHAR(30)  NOT NULL,
+    email    VARCHAR(254) NOT NULL,
+    PRIMARY KEY (username),
+    UNIQUE (email)
+);
+
+CREATE TABLE wish_list
+(
+    id          INT          NOT NULL AUTO_INCREMENT,
+    username    VARCHAR(15)  NOT NULL,
+    title       VARCHAR(100) NOT NULL,
+    description VARCHAR(800) NULL,
+    eventdate   DATE         NULL,
+    not_public  BOOLEAN      NOT NULL DEFAULT 0,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_wish_list_user_account FOREIGN KEY (username)
+        REFERENCES user_account (username)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE wish_item
+(
+    id           INT            NOT NULL AUTO_INCREMENT,
+    wish_list_id INT            NOT NULL,
+    title        VARCHAR(100)   NOT NULL,
+    favourite    BOOLEAN        NOT NULL DEFAULT 0,
+    description  VARCHAR(800)   NULL,
+    price        DECIMAL(10, 2) NULL,
+    quantity     INT            NOT NULL DEFAULT 1,
+    link         VARCHAR(400)   NULL,
+    reserved     BOOLEAN        NOT NULL DEFAULT 0,
+    reserved_by  VARCHAR(15)    NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_wish_list_id FOREIGN KEY (wish_list_id)
+        REFERENCES wish_list (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT fk_wish_item_reserved_by FOREIGN KEY (reserved_by)
+        REFERENCES user_account (username)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
 
 -- creating test users
 INSERT INTO user_account
-VALUES
-    ('test_wisher', 'test123', 'wisher@test.dk'),
-    ('test_wishee', 'test123', 'wishee@test.dk');
+VALUES ('test_wisher', 'test123', 'wisher@test.dk'),
+       ('test_wishee', 'test123', 'wishee@test.dk');
 
 -- creating wish lists
 INSERT INTO wish_list (username, title)
-VALUES
-    ('test_wisher','simpel');
+VALUES ('test_wisher', 'simpel');
 
 INSERT INTO wish_list (username, title, description, eventdate)
-VALUES (
-           'test_wisher',
-           'komplet',
-           'ønskeseddel til fødselsdag',
-           '2025-12-24'
-       );
+VALUES ('test_wisher',
+        'komplet',
+        'ønskeseddel til fødselsdag',
+        '2025-12-24');
+
+INSERT INTO wish_list (username, title, description, eventdate, not_public)
+VALUES ('test_wisher',
+        'privat',
+        'ønskeseddel til fødselsdag',
+        '2025-12-24',
+        1);
 
 -- creating wishes in wish list 1
-INSERT INTO wish_item (wish_list_id, title) VALUES (1,'Verdensfred');
+INSERT INTO wish_item (wish_list_id, title)
+VALUES (1, 'Verdensfred');
 INSERT INTO wish_item (wish_list_id, title, favourite, description, price, quantity, link, reserved, reserved_by)
-VALUES (
-           1,
-           'Fjernstyret bil',
-           1,
-           'bil der kan fjernstyres - SKAL være blå',
-           239.00,
-           1,
-           'https://www.avxperten.dk/maxlife-rc-fjernstyret-bil-mxrc-300.asp',
-           1,
-           'test_wishee'
-       );
+VALUES (1,
+        'Fjernstyret bil',
+        1,
+        'bil der kan fjernstyres - SKAL være blå',
+        239.00,
+        1,
+        'https://www.avxperten.dk/maxlife-rc-fjernstyret-bil-mxrc-300.asp',
+        1,
+        'test_wishee');
 
 -- creating wishes in wish list 2
-INSERT INTO wish_item (wish_list_id, title) VALUES (2,'Verdensfred');
+INSERT INTO wish_item (wish_list_id, title)
+VALUES (2, 'Verdensfred');
 INSERT INTO wish_item (wish_list_id, title, favourite, description, price, quantity, link, reserved, reserved_by)
-VALUES (
-           2,
-           'Fjernstyret bil',
-           1,
-           'bil der kan fjernstyres - SKAL være blå',
-           239.00,
-           1,
-           'https://www.avxperten.dk/maxlife-rc-fjernstyret-bil-mxrc-300.asp',
-           1,
-           'test_wishee'
-       );
+VALUES (2,
+        'Fjernstyret bil',
+        1,
+        'bil der kan fjernstyres - SKAL være blå',
+        239.00,
+        1,
+        'https://www.avxperten.dk/maxlife-rc-fjernstyret-bil-mxrc-300.asp',
+        1,
+        'test_wishee');
+
+-- creating wishes in wish list 2
+INSERT INTO wish_item (wish_list_id, title)
+VALUES (3, 'Verdensfred');
+INSERT INTO wish_item (wish_list_id, title, favourite, description, price, quantity, link, reserved, reserved_by)
+VALUES (3,
+        'Fjernstyret bil',
+        1,
+        'bil der kan fjernstyres - SKAL være blå',
+        239.00,
+        1,
+        'https://www.avxperten.dk/maxlife-rc-fjernstyret-bil-mxrc-300.asp',
+        1,
+        'test_wishee');
