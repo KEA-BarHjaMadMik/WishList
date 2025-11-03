cde48a03b097336109d171ef704ba0ba9bc248c7
HjalteMuus <hjaltemb2@gmail.com>
2025-11-03 12:23:05 +0100

    shouldGetUserWishLists test made

--- src/test/java/com/example/wishlist/controller/WishListControllerTest.java
+++ src/test/java/com/example/wishlist/controller/WishListControllerTest.java
@@ -6,14 +6,18 @@ import com.example.wishlist.service.WishListService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(WishListController.class)
 class WishListControllerTest {
@@ -23,17 +27,17 @@ class WishListControllerTest {
     @MockitoBean
     private WishListService wishListService;
 
-    @Mock
-    private MockHttpSession mockHttpSession;
-
     private User user;
-    private WishList wishList;
-    private LocalDate localDate = LocalDate.parse("2030-12-12");
-        //wishList = new WishList(1,"user", "test", "", localDate, false);
+    private List<WishList> wishList;
+    private LocalDate eventDate;
 
     @BeforeEach
     void setUp() {
         user = new User("tester", "test123", "test@gmail.com");
+        eventDate = LocalDate.parse("2030-12-12");
+        wishList.add(new WishList(1, "tester", "test", "1", eventDate, false));
+        wishList.add(new WishList(2, "tester", "test2", "2", eventDate, false));
+
     }
 
     @AfterEach
@@ -41,8 +45,15 @@ class WishListControllerTest {
     }
 
     @Test
-    void shouldGetWishLists() throws Exception{
+    void shouldGetUserWishLists() throws Exception {
+        when(wishListService.getUserWishLists("tester")).thenReturn(wishListService.getUserWishLists("tester"));
 
+        mockMvc.perform(get("/wish_lists"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("wish_lists"))
+                .andExpect(model().attributeExists("userWishLists"));
+
+        verify(wishListService).getUserWishLists("tester");
     }
 
 
